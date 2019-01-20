72e1a32d17fa98eb0c44949229dd5209fac2822b
JhonKeven <1217750982@qq.com>
2019-01-20 23:12:38 +0800

    集群版配置中心

--- config-server/src/main/java/com/forezp/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/forezp/configserver/ConfigServerApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBoot Application
 @EnableConfigServer
+@EnableEureka
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
